e70bbc2d59cc991f387830a4bd333ce2bc4ea5fd
harsh verma <harshverma0362@gmail.com>
2021-04-03 23:55:21 +0530

    products lists admin screenimplimented server side

--- client/src/Screens/AdminProductsScreeen.js
+++ client/src/Screens/AdminProductsScreeen.js
@@ -11,10 +11,14 @@ import {
   TableRow,
   Typography,
 } from "@material-ui/core";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Delete, Edit } from "@material-ui/icons";
+import Loader from "../Components/Loader";
+import { Alert } from "@material-ui/lab";
+import { getProductsList } from "../actions/productActions";
 
 const AdminProductsScreeen = ({ history }) => {
+  const dispatch = useDispatch();
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
@@ -24,55 +28,63 @@ const AdminProductsScreeen = ({ history }) => {
   useEffect(() => {
     if (!userInfo || !userInfo.isAdmin) {
       history.push("/login");
+    } else {
+      dispatch(getProductsList());
     }
   }, [userInfo, history]);
   return (
     <Grid container justify="center" style={{ marginTop: 16 }}>
-      <Grid item xs={10}>
+      <Grid item xs={12}>
         <Typography variant="h3">{"Products"}</Typography>
       </Grid>
-      <Grid item xs={10} style={{ marginTop: 10 }}>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">ID</TableCell>
-                <TableCell align="center">Name</TableCell>
-                <TableCell align="center">In Stock</TableCell>
-                <TableCell align="center">Price</TableCell>
-                <TableCell align="center">Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {products.map((product) => (
-                <TableRow key={product._id}>
-                  <TableCell align="center">{product._id}</TableCell>
-                  <TableCell align="center">{product.name}</TableCell>
-                  <TableCell align="center">
-                    {product.countInStock > 0
-                      ? product.countInStock
-                      : "OUT OF STOCK"}
-                  </TableCell>
-                  <TableCell align="center">{product.price}</TableCell>
-                  <TableCell align="center">
-                    <ButtonGroup
-                      size="small"
-                      aria-label="small outlined button group"
-                    >
-                      <Button variant="contained" color="secondary">
-                        <Edit />
-                      </Button>
-                      <Button variant="contained" color="primary">
-                        <Delete />
-                      </Button>
-                    </ButtonGroup>
-                  </TableCell>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Alert severity="error">{error}</Alert>
+      ) : (
+        <Grid item xs={10} style={{ marginTop: 10 }}>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">ID</TableCell>
+                  <TableCell align="center">Name</TableCell>
+                  <TableCell align="center">In Stock</TableCell>
+                  <TableCell align="center">Price</TableCell>
+                  <TableCell align="center">Actions</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Grid>
+              </TableHead>
+              <TableBody>
+                {products.map((product) => (
+                  <TableRow key={product._id}>
+                    <TableCell align="center">{product._id}</TableCell>
+                    <TableCell align="center">{product.name}</TableCell>
+                    <TableCell align="center">
+                      {product.countInStock > 0
+                        ? product.countInStock
+                        : "OUT OF STOCK"}
+                    </TableCell>
+                    <TableCell align="center">{product.price}</TableCell>
+                    <TableCell align="center">
+                      <ButtonGroup
+                        size="small"
+                        aria-label="small outlined button group"
+                      >
+                        <Button variant="contained" color="secondary">
+                          <Edit />
+                        </Button>
+                        <Button variant="contained" color="primary">
+                          <Delete />
+                        </Button>
+                      </ButtonGroup>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      )}
     </Grid>
   );
 };
