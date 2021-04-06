c61bed1d940184e7f3a085d1ca7a4091d4162260
Harsh Verma <harshverma0362@gmail.com>
2021-04-06 22:08:33 +0530

    delete product by id inplimented in server side

--- server/controller/productsController.js
+++ server/controller/productsController.js
@@ -88,3 +88,18 @@ export const updateProduct = asyncHandler(async (req, res) => {
     throw new Error("Product not found");
   }
 });
+
+// DESC => Delete Product By Id
+// ROUTE => DELETE /api/products/:id
+// ACCESS => Private/Admin
+export const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.deleteOne();
+    res.status(200).send("Delete Successfully");
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -5,10 +5,15 @@ import {
   getProductById,
   addProduct,
   updateProduct,
+  deleteProduct,
 } from "../controller/productsController.js";
 import { protect, admin } from "../middlewares/authMiddleware.js";
 
 router.route("/").get(getProductsList).post(protect, admin, addProduct);
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 export default router;
